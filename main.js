719044acf166186c14cb9d7e7d4b8ee154eee81c
unknown <vikasvarshney767@gmail.com>
2024-04-11 19:02:30 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,32 @@
+window.onload = () => {
+    createCanvas(640, 480);
+
+    const triangulation = new Delunay_Triangulation();
+
+    // Array of points that will be added to the Triangulation
+    let points = [];
+
+    document.getElementById('canvas').addEventListener('click', () => {
+        points.push(MOUSE_POS);
+
+        // Clear screen
+        clear('white');
+
+        // Draw Points
+        for (let point of points) {
+            drawFillRect(point.x - 2.5, point.y - 2.5, 5, 5, 'red');
+        }
+        
+        let triangles = triangulation.RunAlgorithm(points);
+
+        // Draw the triangles
+        // TODO - filter the edges and draw them only once
+        for (let triangle of triangles) {
+            triangle.draw();
+            // triangle.drawFill('cyan', true);
+            // triangle.drawCircumcircle();
+        }
+
+        console.log(`Number of triangles: ${triangles.length}`);
+    });
+};
